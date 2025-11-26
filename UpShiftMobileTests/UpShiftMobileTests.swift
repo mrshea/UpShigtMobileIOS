5d8c96bb4e0d23af29c55aa2b1f56246bca84373
Michael Shea <michaelshea123@gmail.com>
2025-11-26 11:46:47 -0800

    Initial Commit

---
+++ UpShiftMobileTests/UpShiftMobileTests.swift
@@ -0,0 +1,16 @@
+//
+//  UpShiftMobileTests.swift
+//  UpShiftMobileTests
+//
+//  Created by Michael Shea on 11/26/25.
+//
+
+import Testing
+
+struct UpShiftMobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
